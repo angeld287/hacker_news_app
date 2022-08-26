290a1cbdbb3cd17b1e1eff3f667132d9fb68ca39
Angel Angeles <daniel_1234@hotmail.es>
2022-08-26 04:09:56 -0400

    adding service response interface structure

---
+++ src/interfaces/IHit.ts
@@ -0,0 +1,36 @@
+export default interface IHit {
+    created_at: string;
+    title: string;
+    url: string;
+    author: string;
+    points: string;
+    story_text: string;
+    comment_text: string;
+    num_comments: number;
+    story_id: number;
+    story_title: string;
+    story_url: string;
+    parent_id: number;
+    created_at_i: number;
+    _tags: string[];
+    objectID: string;
+    _highlightResult: IHighlightResult
+
+}
+
+interface IHighlightResult {
+    author: ICommun;
+    comment_text: ICommentText;
+    story_title: ICommun;
+    story_url: ICommun;
+}
+
+interface ICommentText extends ICommun {
+    fullyHighlighted: boolean;
+}
+
+interface ICommun {
+    value: string;
+    matchLevel: string;
+    matchedWords: string[];
+}
