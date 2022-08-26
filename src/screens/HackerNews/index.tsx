45da5fd37593437387d2c4e9a1065b08652c8e39
Angel Angeles <daniel_1234@hotmail.es>
2022-08-26 06:04:41 -0400

    adding header in hacker news screen

---
+++ src/screens/HackerNews/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+const HackerNews: React.FC = () => {
+
+    return (
+        <div className="Front-End-Test---Home-view">
+            <div className="Rectangle-2-Copy">
+                <span className="HACKER-NEWS">
+                    HACKER NEWS
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default HackerNews
