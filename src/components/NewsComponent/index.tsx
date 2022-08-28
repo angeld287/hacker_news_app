5c5236dcc6037216b0ad8359c701e33a5e6bb8a8
Angel Angeles <daniel_1234@hotmail.es>
2022-08-28 19:01:07 -0400

    adding href to blank page

--- src/components/NewsComponent/index.tsx
+++ src/components/NewsComponent/index.tsx
@@ -9,20 +9,23 @@ const NewsComponent: React.FC<IHitComponent> = ({ author, story_title, story_url
     useEffect(() => {
         setTime(timeSince(new Date(created_at)));
     }, [created_at])
+
     return (
         <div className="news-component">
             <div className="news-column column-body">
-                <div className="time-row">
-                    <div className="time-column clock-icon">
-                        <div className="clock-icon">
-                            <img alt="" src="https://github.com/angeld287/hacker_news_app/blob/master/src/assets/iconmonstr-time-2.png?raw=true" className="iconmonstr-time-2" />
+                <a href={story_url} target="_blank" rel="noopener noreferrer">
+                    <div className="time-row">
+                        <div className="time-column clock-icon">
+                            <div className="clock-icon">
+                                <img alt="" src="https://github.com/angeld287/hacker_news_app/blob/master/src/assets/iconmonstr-time-2.png?raw=true" className="iconmonstr-time-2" />
+                            </div>
                         </div>
+                        <div className="time-column time-description">{time} ago by {author}</div>
                     </div>
-                    <div className="time-column time-description">{time} ago by {author}</div>
-                </div>
-                <div className="title-row">
-                    <b>{story_title}</b>
-                </div>
+                    <div className="title-row">
+                        <b>{story_title}</b>
+                    </div>
+                </a>
             </div>
             <div className="news-column actions">
                 {!isInMyFaves &&
