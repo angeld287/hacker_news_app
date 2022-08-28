39a3b7f7e2ecac473aad576691fe1ac4a7b9cafe
Angel Angeles <daniel_1234@hotmail.es>
2022-08-28 17:18:56 -0400

    news component design finished

--- src/components/NewsComponent/index.tsx
+++ src/components/NewsComponent/index.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IHitComponent } from '../../interfaces/components/IHits';
 import { Favorite, FavoriteBorder } from '@mui/icons-material';
 import './styles.css';
+import { timeSince } from '../../utils/tools';
 
 const NewsComponent: React.FC<IHitComponent> = ({ author, story_title, story_url, created_at, addToFaves, removeFromFaves, isInMyFaves }) => {
-
+    const [time, setTime] = useState("")
+    useEffect(() => {
+        setTime(timeSince(new Date(created_at)));
+    }, [created_at])
     return (
         <div className="news-component">
             <div className="news-column column-body">
                 <div className="time-row">
-                    <div className="clock-icon">
+                    <div className="time-column clock-icon">
                         <div className="clock-icon">
                             <img alt="" src="https://github.com/angeld287/hacker_news_app/blob/master/src/assets/iconmonstr-time-2.png?raw=true" className="iconmonstr-time-2" />
                         </div>
                     </div>
-                    <div className="time-description">{created_at} {author}</div>
+                    <div className="time-column time-description">{time} ago by {author}</div>
                 </div>
                 <div className="title-row">
                     <b>{story_title}</b>
