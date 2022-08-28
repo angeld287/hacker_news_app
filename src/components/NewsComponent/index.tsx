da8a495e70a2d46e0f91492c6319b716fa41c7bd
Angel Angeles <daniel_1234@hotmail.es>
2022-08-28 14:35:33 -0400

    adding hart buttons design

--- src/components/NewsComponent/index.tsx
+++ src/components/NewsComponent/index.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { IHitComponent } from '../../interfaces/components/IHits';
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
 import './styles.css';
 
 const NewsComponent: React.FC<IHitComponent> = ({ author, story_title, story_url, created_at, addToFaves, removeFromFaves, isInMyFaves }) => {
 
     return (
         <div className="news-component">
-            <a target="_blank" rel="noreferrer" href={story_url}>{story_title}</a>
-            {created_at}
-            {author}
-            {!isInMyFaves && <button onClick={addToFaves}>Add to Fav</button>}
-            {isInMyFaves && <button onClick={removeFromFaves}>Remove from Fav</button>}
+            <div className="news-column body">
+                <div className="time-row">
+                    <div className="clock-icon"></div>
+                    <div className="time-description">{created_at} {author}</div>
+                </div>
+                <div className="title-row">
+                    {story_title}
+                </div>
+            </div>
+            <div className="news-column actions">
+                {!isInMyFaves &&
+                    <button onClick={addToFaves}>
+                        <FavoriteBorder sx={{ color: 'red' }} />
+                    </button>
+                }
+                {isInMyFaves &&
+                    <button onClick={removeFromFaves}>
+                        <Favorite sx={{ color: 'red' }} />
+                    </button>
+                }
+            </div>
         </div>
     )
 }
