9fa75b8b8a0928ae1507666ed66782c95544fa84
Angel Angeles <daniel_1234@hotmail.es>
2022-08-28 17:50:07 -0400

    adding select tech assets

--- src/components/SelectComponent/index.tsx
+++ src/components/SelectComponent/index.tsx
@@ -3,18 +3,43 @@ import { useAppSelector } from '../../app/hooks';
 import { selectSearch } from '../../features/finder/searchSlice';
 import { ISelect } from '../../interfaces/components/ISelect';
 import './styles.css';
+import { Favorite } from '@mui/icons-material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
 
 const SelectComponent: React.FC<ISelect> = ({ placeholder, options, onChange }) => {
     const search = useAppSelector(selectSearch);
+    const [value, setValue] = React.useState('');
+
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setValue(event.target.value);
+    };
+
 
     if (!search.newsType)
         return <h1>Cargando...</h1>;
 
     return (
         <div className="select-body">
-            <select className="select" defaultValue={search.newsType} onChange={onChange} placeholder={placeholder} name="news-categoy" >
-                {options.map(option => <option key={`key-${option.value}`} value={option.value}>{option.text}</option>)}
-            </select>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <Select
+                    value={value}
+                    onChange={handleChange}
+                    displayEmpty
+                    inputProps={{ 'aria-label': 'Without label' }}
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}><Favorite />Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
         </div>
 
     )
